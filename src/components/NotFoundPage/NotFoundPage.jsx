01dde3087d14ab33194db4e68a916035edf5b033
Muslim Tsokaev <47528621+katada93@users.noreply.github.com>
2021-05-21 11:24:38 +0300

    refactor: change styles for startPage and notFoundPage

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import styles from './NotFoundPage.module.css';
-import userIcon from '../../images/user-big.svg';
+import React from "react";
+import styles from "./NotFoundPage.module.css";
+import userIcon from "../../images/user-big.svg";
 
 const NotFoundPage = () => {
   return (
     <div className={styles.error}>
-      <img className={styles.errorImg} src={userIcon} alt='Search Icon' />
-      <p className='startText'>User not found</p>
+      <div className={styles.content}>
+        <img className={styles.errorImg} src={userIcon} alt="Search Icon" />
+        <p className="startText">User not found</p>
+      </div>
     </div>
   );
 };
--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import styles from './StartPage.module.css';
-import searchIcon from '../../images/search.svg';
+import React from "react";
+import styles from "./StartPage.module.css";
+import searchIcon from "../../images/search.svg";
 
 const StartPage = () => {
   return (
     <div className={styles.start}>
-      <img className={styles.startImg} src={searchIcon} alt='Search Icon' />
-      <p className='startText'>Start with searching a GinHub user</p>
+      <div className={styles.content}>
+        <img className={styles.startImg} src={searchIcon} alt="Search Icon" />
+        <p className="startText">Start with searching a GinHub user</p>
+      </div>
     </div>
   );
 };
